42542643a1329e6daac760a6d0dbd2e61c54eba1
kinny <1549722424@qq.com>
2016-12-18 17:29:12 +0800

    增加一个标记过得人脸数据库表

--- Crawler/schema.sql
+++ Crawler/schema.sql
@@ -32,3 +32,13 @@ CREATE TABLE `Tbl_Huajiao_User` (
     `FScrapedTime` timestamp NOT NULL COMMENT '爬虫时间',
     PRIMARY KEY (`FUserId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+DROP TABLE IF EXISTS `Tbl_Beauty_Face`;
+CREATE TABLE `Tbl_Beauty_Face` (
+    `FFaceId` INT UNSIGNED  AUTO_INCREMENT NOT NULL,
+    `FImageUrl` VARCHAR(255) NOT NULL DEFAULT '' COMMENT '图片URL',
+    `FImageOriginal` VARCHAR(255) NOT NULL DEFAULT '' COMMENT '原始图片路径',
+    `FImageFiltered` VARCHAR(255) NOT NULL DEFAULT '' COMMENT '过滤后图片路径',
+    `FImageLandmarks` JSON NOT NULL COMMENT '人脸识别的各种特征点',
+    PRIMARY KEY (`FFaceId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
